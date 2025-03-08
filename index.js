0ac69ee6633585ae7fc000e026ad9984aaf20671
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-03-08 17:29:07 -0600

    corección dela instancia del servidor

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const express = require('express');
+const express = require('express')
 
-const app = Express();
+const app = express()
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+})
