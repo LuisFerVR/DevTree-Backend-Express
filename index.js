03f805f7b90bd282e643f72b3f7f88b697081f45
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-03-08 18:12:05 -0600

    de commonjs a ecmascript (module) en el package.json y en la sintaxis del index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express')
+//const express = require('express'); // CJS CommonJS
+import express from 'express'; // ESM EcmaScript Module
+const app = express();
 
-const app = express()
+app.get('/', (req, res) => {
+    res.send('Hello World desde express')
+})
+
+app.get('/ecommerce', (req, res) => {
+    res.send('Hello World desde ecommerce')
+})
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
