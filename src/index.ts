984c80798bfa7b7aa8daffa50b8029cacfa8b8cb
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-03-14 11:49:34 -0600

    Corrección para leer datos de formulario

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import server from './server';
 
 const port = process.env.port || 4000;
-server.listen(server.port, () => {
-    console.log('Servidor corriendo en el puerto:', port);
-})
+try {
+    server.listen(port, '0.0.0.0', () => {
+      console.log(`Servidor corriendo en el puerto: ${port}`);
+    });
+  } catch (error) {
+    console.error('Error al iniciar el servidor:', error);
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express'; // ESM EcmaScript Module
 import router from './router'; // Importamos el router
+
 const app = express();
 
+app.use(express.json()); // Leer datos en json
 app.use('/', router); // Usamos el router en la ruta raíz
-app.use(express.json()); // Middleware para parsear JSON
 
 
 export default app; // Exportamos la app para poder importarla en otro archivo
