a17620acb395c3cb530e73d8795453dbf5cfed68
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-03-17 13:43:31 -0600

    Validando ususarios existentes por el correo y devolviendo estatus y mensajes personalizados.

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -5,6 +5,13 @@ export const UserAccount = async (req:Request, res:Response) => {
 
     //await User.create(req.body);
 
+    const userExist = await User.findOne({email: req.body.email});
+
+    if (userExist) {
+        const error = new Error("El usuario ya existe");
+        return res.status(409).json({error: error.message});
+    }
+
     const user = new User(req.body);
     await user.save();
 
