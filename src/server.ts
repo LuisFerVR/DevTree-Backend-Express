f3d9dd32b34fec532a3098721c325b5b9966d5e5
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-03-22 19:50:56 -0600

    instalación de librería cors, creación de su archivo de configuración y aplicación en server.ts que es en donde se usa el middleware app.use(cors(corsConfig))

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express'; // ESM EcmaScript Module
+import cors from 'cors'; // Importamos cors
 import 'dotenv/config'; // Importamos dotenv para poder usar las variables de entorno
 import router from './router'; // Importamos el router
 import { connectBD } from './config/db';
-
-const app = express();
+import { corsConfig } from './config/cors';
 
 connectBD(); // Conectamos a la base de datos
+
+const app = express();
+app.use(cors(corsConfig))
+
 app.use(express.json()); // Leer datos en json
 app.use('/', router); // Usamos el router en la ruta raíz
 
