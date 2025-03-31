33473e841afc72ad7a73688f3815ea2217d44338
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-03-31 12:13:06 -0600

    Se modificó el modelo en el ORM agregando el campo links de tipo string. Se guardó en el modelo los links recibidos del front en el handler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -59,7 +59,7 @@ export const getUser = async (req:Request, res:Response) => {
 
 export const updateProfile = async (req:Request, res:Response) => {
     try {
-        const {description} = req.body;
+        const {description, links} = req.body;
 
         const handle = slug(req.body.handle,'');
         const handleExist = await User.findOne({handle});
@@ -71,6 +71,7 @@ export const updateProfile = async (req:Request, res:Response) => {
         //Actualizar el perfil del usuario
         req.user.handle = handle;
         req.user.description = description;
+        req.user.links = links;
         await req.user.save();
         res.send("Peril actaulizado correctamente");
         
